6e6c3e706f54631611844dc27d8ea867e9ac314d
Roman <me@mail.ru>
2019-07-26 15:44:06 +0300

    final

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,39 @@
 const my_func = (function (){
-    let main_display = document.querySelector(".main-display");
+    this.main_display = document.querySelector(".main-display");
     let screen_height = $(window).height(); 
     console.log(screen_height);
-    main_display.style.height = `${screen_height}px`;
+    this.main_display.style.height = `${screen_height}px`;
 })
 
 my_func();
 
+
+/* */
+const hide_head = (function(){
+  window.onscroll = function() {
+    if (window.pageYOffset > 0) {
+      $( ".slide-header" ).fadeOut( 200, 'linear');
+      $()
+    } else {
+      $( ".slide-header" ).fadeIn();
+    }
+  };
+})
+
+hide_head();
 /*top slider */
+
+$(document).ready(function(){
+    $('.main-slider').slick({
+        dots: true,
+        arrows: true,
+        adaptiveHeight: true,
+        prevArrow: '<button class="top-custom-btn slick-prev"><i class="fas fa-long-arrow-alt-left"></i></button>',
+        nextArrow: '<button class="top-custom-btn slick-next"><i class="fas fa-long-arrow-alt-right"></i></button>'
+    });
+  });
+
+/*bottom slider*/ 
 
 $(document).ready(function(){
     $('.bottom-slider').slick({
@@ -15,4 +41,26 @@ $(document).ready(function(){
         nextArrow: '<button class="custom-btn slick-next"><i class="fas fa-arrow-right"></i></button>'
     });
   });
-                  
+
+
+/*   $(document).ready(function(){
+    let arr = document.querySelectorAll(".slick-slide")
+    setTimeout(() => {
+      arr.forEach(el => {
+        el.style.height = $(window).height();
+      });
+      console.log(1)
+    }, 4000);
+    
+    }
+  ); */
+
+  /* responsive-menu */
+  $(document).ready(function(){
+    $('.open-responsive-menu').click(function(){
+      $('.responsive-menu').fadeIn()
+    });
+    $('.close').click(function(){
+      $('.responsive-menu').fadeOut()
+    })
+  })
